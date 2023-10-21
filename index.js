c6e3ca44181261287bf0470c7f92e5bd2d48dcee
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2023-10-21 16:54:19 -0500

    Api Express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const debug = require('debug')('app:main')
+
+const { config } = require('./src/config/configIndex')
+const { productsApi } = require('./src/products/productsIndex')
+const { usersApi } = require('./src/users/userIndex')
+const { avaliableRoutes, invalidRoutes } = require('./src/indexRoutes/indexRoutes')
+
+const app = express()
+app.use(express.json())
+avaliableRoutes(app)
+usersApi(app)
+productsApi(app)
+invalidRoutes(app)
+
+app.listen(config.port, () => {
+  debug('Listening on port http://localhost:' + config.port)
+})
